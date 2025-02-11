0c3b2a94c030817a5e4a56d0b9521b04e7ce105c
Collo-V <collinsvictoromondi@gmail.com>
2025-02-11 22:48:12 +0300

    Add forgotten rollup.config.js in first commit

---
+++ rollup.config.js
@@ -0,0 +1,76 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import postcss from 'rollup-plugin-postcss';
+import del from 'rollup-plugin-delete';
+import babel from '@rollup/plugin-babel';
+import typescript from 'rollup-plugin-typescript2'
+import {createRequire} from 'node:module'
+import json from '@rollup/plugin-json'
+import alias from '@rollup/plugin-alias';
+import { terser } from 'rollup-plugin-terser';
+import * as path from 'node:path'
+import { fileURLToPath } from 'url';
+const require = createRequire(import.meta.url);
+const packageJson = require('./package.json');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const extensions = ['.js', '.jsx', '.ts', '.tsx']
+export default {
+  input: 'src/index.ts',
+  output: [
+    // {
+    //   file: packageJson.main,
+    //   format: 'cjs',
+    //   sourcemap: true,
+    //
+    // },
+    {
+      file: packageJson.module,
+      format: 'esm',
+      inlineDynamicImports:true,
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    alias({
+      entries:[
+        { find: '@', replacement: path.resolve(__dirname, './src') },
+      ]
+    }),
+    peerDepsExternal(),
+    json(),
+    resolve({
+      extensions
+    }),
+    commonjs(),
+    typescript({
+      check:false,
+      tsconfig:'./rollup.tsconfig.json',
+    }),
+    babel({
+      exclude: 'node_modules/**',
+      babelHelpers: 'bundled',
+      extensions
+    }),
+    del({ targets: 'dist/*' }),
+    postcss({
+      config:'./postcss.config.cjs',
+      // modules: true,
+      extract: 'styles.css',
+      sourceMap: true,
+      plugins: [
+        require('postcss-preset-env')({
+          features: {
+            // ... other features
+            // 'nesting': true,
+            // 'tailwindcss': true,
+          },
+        }),
+
+      ],
+    }),
+    terser()
+  ],
+  external:['react','react-dom','next','react-icons','antd','@react-pdf/renderer','react-share'],
+}
